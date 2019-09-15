f8f60603adaac282ed6af714d614017fac43d818
piyumisudusinghe <piyumisudusinghe@gmail.com>
2019-09-15 21:30:17 +0530

    Initial commit

---
+++ app/src/main/java/com/piyumi/locationtracker/GPSTracker.java
@@ -0,0 +1,70 @@
+package com.piyumi.locationtracker;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+public class GPSTracker implements LocationListener {
+    Context context;
+
+    public GPSTracker(Context c){
+        this.context = c;
+
+    }
+
+    public Location getLocation(){
+
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ){
+            Toast.makeText(context,"Permission not granted",Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+
+        if(isGPSEnabled){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,6000,15,this);
+
+            if(locationManager != null){
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                return location;
+            }
+
+        }else{
+            Toast.makeText( context,"Please enable GPS",Toast.LENGTH_LONG).show();
+        }
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(!MainActivity.isPressedStop){
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            Toast.makeText(context,"LAT : " + lat + "\n LON : " + lon, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
---
+++ app/src/main/java/com/piyumi/locationtracker/MainActivity.java
@@ -0,0 +1,62 @@
+package com.piyumi.locationtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class  MainActivity extends AppCompatActivity {
+
+    Button btnGetLocation;
+
+    Button btnStop;
+
+    public static boolean isPressedStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetLocation = (Button)findViewById(R.id.btnGetLocation);
+        btnStop = (Button)findViewById(R.id.btnStop);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION
+        },123);
+
+        btnGetLocation.setOnClickListener( new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+                isPressedStop = false;
+                GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
+                Location location = gpsTracker.getLocation();
+                if(location != null){
+                    Toast.makeText(getApplicationContext(),"Start getting location updates", Toast.LENGTH_LONG).show();
+                    double lat = location.getLatitude();
+                    double lon = location.getLongitude();
+                    Toast.makeText(getApplicationContext(),"LAT : " + lat + "\n LON : " + lon, Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Location null piyumi", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPressedStop = true;
+                Toast.makeText(getApplicationContext(),"Stop getting location updates", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
